b1302f1eef776f5ec0cb1c7cdc6e6a7f12a1c6a2
Stav Armoni-Friedmann <stav.armoni-friedmann@kcl.ac.uk>
2022-11-09 18:49:15 +0000

    Created Student model and sign_up form

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 # Create your models here.
+class Student(AbstractUser):
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+
+    email = models.EmailField()
+
+    first_name = models.CharField(max_length=50)
+
+    last_name = models.CharField(max_length=50)
+
+    password = models.CharField()
