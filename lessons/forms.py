56cafe3cbb682086b0cb8f0c551bfd17f3afd6d6
Stav Armoni-Friedmann <stav.armoni-friedmann@kcl.ac.uk>
2022-11-14 19:16:54 +0000

    Add constraints to Student model and relevant tests
    - Username must be unique and begin with an @
    - Username must contain at least 3 alphanumericals after @
    - Password is hashed
    - Email must be unique

--- lessons/forms.py
+++ lessons/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import RegexValidator
 from lessons.models import Student
 from django.urls import reverse_lazy
 from crispy_forms.helper import FormHelper
@@ -30,8 +31,16 @@ class SignUpForm(forms.ModelForm):
         model = Student
         fields = ['first_name', 'last_name', 'email', 'username','new_password','confirm_password']
 
-    new_password = forms.CharField(label='New Password', widget=forms.PasswordInput)
-    confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[RegexValidator(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+            message='Password must contain an uppercase character, a lowercase '
+                    'character and a number'
+        )]
+    )
+    confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput())
 
 
     def clean(self):
