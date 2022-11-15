ce6ad45f20dbf617624a843559a4d07299acc2e9
Mudassir Ahmad <smahmad2002@gmail.com>
2022-11-15 16:28:01 +0000

    improvements to UI

--- lessons/forms.py
+++ lessons/forms.py
@@ -65,7 +65,7 @@ class StandardForm():
 
         for field in fields:
             helper.layout.append(
-                Field(field,css_class = "bg-transparent text-light")
+                Field(field,css_class = "bg-transparent text-light mb-2")
             )
         helper.layout.append(Submit(submitName,submitValue))
         return helper
