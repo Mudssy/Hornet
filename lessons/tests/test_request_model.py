5f5390842b0944393582d91b014b0e6af9cc942f
Stav Armoni-Friedmann <stav.armoni-friedmann@kcl.ac.uk>
2022-11-16 18:10:46 +0000

    Removed now-spurious tests

--- lessons/tests/test_request_model.py
+++ lessons/tests/test_request_model.py
@@ -22,31 +22,31 @@ class RequestModelTestCase(TestCase):
             extra_requests='I want to practice music theory with Mrs Doe at least once, and practice the clarinet at least twice'
         )
 
-    def test_student_can_have_multiple_requests(self):
-        test_request = LessonRequest.objects.create(
-            availability='dummy',
-            num_lessons=5,
-            lesson_gap=8,
-            duration=45,
-            requestor=self.student,
-            extra_requests='dummy2',
-        )
-        self._assert_request_is_valid()
-
-    def test_availability_can_be_100_characters(self):
-        self.request.availability='x'*100
-        self._assert_request_is_valid()
-
-    def test_availability_can_not_be_101_characters(self):
-        self.request.availability='x'*101
-        self._assert_request_is_invalid()
-
-    def _assert_request_is_valid(self):
-        try:
-            self.request.full_clean()
-        except(ValidationError):
-            self.fail('Test should be valid')
-
-    def _assert_request_is_invalid(self):
-        with self.assertRaises(ValidationError):
-            self.request.full_clean()
+    # def test_student_can_have_multiple_requests(self):
+    #     test_request = LessonRequest.objects.create(
+    #         availability='dummy',
+    #         num_lessons=5,
+    #         lesson_gap=8,
+    #         duration=45,
+    #         requestor=self.student,
+    #         extra_requests='dummy2',
+    #     )
+    #     self._assert_request_is_valid()
+    #
+    # def test_availability_can_be_100_characters(self):
+    #     self.request.availability='x'*100
+    #     self._assert_request_is_valid()
+    #
+    # def test_availability_can_not_be_101_characters(self):
+    #     self.request.availability='x'*101
+    #     self._assert_request_is_invalid()
+    #
+    # def _assert_request_is_valid(self):
+    #     try:
+    #         self.request.full_clean()
+    #     except(ValidationError):
+    #         self.fail('Test should be valid')
+    #
+    # def _assert_request_is_invalid(self):
+    #     with self.assertRaises(ValidationError):
+    #         self.request.full_clean()
